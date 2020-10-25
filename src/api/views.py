bb007b3b1b08c1ca16f3933f4e147909cbce90bb
= <renatoversianidrakk@gmail.com>
2020-10-25 19:17:21 -0300

    Working beta with fully integrated database support / admin creation and more.

--- src/api/views.py
+++ src/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
+from django.utils import timezone
+
 from datetime import datetime
+
+from .models import ScheduledDate
 
 # TODO: Add a view to check which days and hours are already booked
 # TODO: Refactor the code ( there are functions that do way too much stuff )
@@ -66,6 +70,39 @@ def api_schedule(request):
         return redirect('schedule_error', error_code=3)
 
     # TODO: Check if schedule day and hour is available
+    scheduled_dates = ScheduledDate.objects.all()
+
+    sanitized_datetime_object = str(datetime_object).split(' ')
+
+    # print(sanitized_datetime_object)
+
+    for scheduled_date in scheduled_dates:
+        sanitized_scheduled_date = str(scheduled_date).split(' ')
+
+        # print(type(scheduled_date))
+        # print(type(datetime_object))
+
+        # Both sanitized dates have the same format: year-month-day
+        # checking to see if they are in the same day, if so, check for the hour
+        # if the hour is the same, check for the minutes, if the minutes are the same
+        # it cannot be added, since there are already a meeting scheduled to that time
+        if sanitized_scheduled_date[0] == sanitized_datetime_object[0]:
+            sanitized_scheduled_time = str(scheduled_date).split(' ')[1].split('+')[0]
+
+            # Cannot schedule a meeting, because there is a meeting already scheduled
+            if sanitized_scheduled_time == sanitized_datetime_object[1]:
+                return redirect('schedule_error', error_code=4)
+
+    # TODO: Redirect
+    # TODO: Revise the names of the variables
+    # TODO: Avoid race-condition
+    current_timezone = timezone.get_current_timezone()
+    timezone_aware_date = current_timezone.localize(datetime_object)
+
+    new_aware_scheduled_meeting = ScheduledDate(date=timezone_aware_date)
+    new_aware_scheduled_meeting.save()
+
+    print(new_aware_scheduled_meeting.id)
 
     return HttpResponse(f'{day}/{month}/{year} {hours}:{minutes} {datetime_weekday}')
 
