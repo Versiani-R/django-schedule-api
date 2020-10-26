c5a54076c45224d61ab1645176593181fda944bf
= <renatoversianidrakk@gmail.com>
2020-10-26 11:58:13 -0300

    Refactoring is taking place

--- src/api/views.py
+++ src/api/views.py
@@ -6,6 +6,8 @@ from datetime import datetime
 
 from .models import ScheduledDate
 
+from operator import itemgetter
+
 # TODO: Add a view to check which days and hours are already booked ( admin only )
 # TODO: Refactor the code ( there are functions that do way too much stuff )
 
@@ -24,7 +26,7 @@ def api_detail(request, day=1, month=2, year=2000):
     return HttpResponse('Hello! This is the detail page!')
 
 
-def api_schedule(request):
+def handle_request_post_data_to_api_schedule(request):
     # Standard checks
     if ('date' and 'hours' and 'minutes' and 'name') not in request.POST:
         return redirect('schedule_error', error_code=1)
@@ -49,11 +51,37 @@ def api_schedule(request):
     # name of the company for organization's sake
     company_name = request.POST['name']
 
-    # Check if the meeting was scheduled to a saturday/sunday
+    return {
+        'day': day,
+        'month': month,
+        'year': year,
+        'hours': hours,
+        'minutes': minutes,
+        'company_name': company_name,
+    }
+
+
+def api_schedule(request):
+
+    post_request = handle_request_post_data_to_api_schedule(request)
+
+    """ Check 1: Checking if the meeting was scheduled to a saturday or to a sunday.
+    
+    The month variable is not 0 index ( html values ... ).
+    Because of that, datetime_string has to consider the month value, and subtract 1.
+    ( months[int(post_request["month"]) - 1] ).
+    
+    All those datetime variables were created in order to check the day of the date object passed through the html.
+    datetime.weekday requires a datetime object and returns an integer.
+    This integer is the 0-index-based value of the days of the week.
+    So, monday = 0, tuesday = 1, ... saturday = 5, sunday = 6.
+    Then, it simply checks if the datetime_weekday variable is greater than 5, if so, it's either a saturday or sunday.
+    """
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
     # months[int(month) - 1] -> months is 0-based while month is not
-    datetime_string = f'{months[int(month) - 1]} {day} {year} {hours}:{minutes}'
+    datetime_string = f'{months[int(post_request["month"]) - 1]} {post_request["day"]} {post_request["year"]} ' \
+                      f'{post_request["hours"]}:{post_request["minutes"]}'
 
     # passing all the variables to a datetime object
     datetime_object = datetime.strptime(datetime_string, '%b %d %Y %H:%M')
@@ -64,24 +92,43 @@ def api_schedule(request):
     if datetime_weekday >= 5:
         return redirect('schedule_error', error_code=2)
 
-    # Check if the meeting was scheduled to the past
+    """ Check 2: Check if the meeting was scheduled to the past
+    
+    It takes usage of the datetime_object previously created to check 1.
+    
+    Datetime objects in general are 'weird'. The newer it is, the greater it is.
+    So, a datetime object for like '2006 Jun 12 13:30PM' is smaller than '2012 Jun 12 13:30PM'.
+    With that in mind, we check for the current date (datetime.now()) and compare with the datetime object.
+    If the current date is greater than the datetime_object, the datetime_object is in the past.
+    """
     current_date = datetime.now()
 
     if current_date > datetime_object:  # AKA, if it's in the past
         return redirect('schedule_error', error_code=3)
 
-    # Check if day and hour is available ( no scheduled meeting for the date and time )
+    """ Check 2: Check if day and hour is available ( no scheduled meeting for the date and time )
+    
+    Retrieve all the scheduled_dates from the database and compare with the datetime_object.
+    If a match on the day is found, it checks for the hour, if a match occurs, then they're scheduled for the same time.
+    
+    sanitized_scheduled_date[0] is the same as the date, it looks like this:
+        * sanitized_scheduled_date[0]:  '2020-10-29'
+        * sanitized_datetime_object[0]: '2020-10-29'
+    
+    sanitized_scheduled_date[1] is the same as the time, it looks like this:
+        * sanitized_scheduled_date[1]:  '12:00:00'
+        * sanitized_datetime_object[1]: '12:00:00'
+        
+    **Note**: Since the user can only schedule a meeting on either 0 or 30 minutes, it's really not necessary to check
+    for hours and only after that, minutes. That's why the time itself is compared, and not hours, and only then 
+    minutes.
+    """
     scheduled_dates = ScheduledDate.objects.all()
 
     sanitized_datetime_object = str(datetime_object).split(' ')
 
-    # print(sanitized_datetime_object)
-
     for scheduled_date in scheduled_dates:
         sanitized_scheduled_date = str(scheduled_date).split(' ')
-
-        # print(type(scheduled_date))
-        # print(type(datetime_object))
 
         # Both sanitized dates have the same format: year-month-day
         # checking to see if they are in the same day, if so, check for the hour
@@ -94,22 +141,21 @@ def api_schedule(request):
             if sanitized_scheduled_time == sanitized_datetime_object[1]:
                 return redirect('schedule_error', error_code=4)
 
-    # TODO: Revise the names of the variables
     # TODO: Avoid race-condition
     current_timezone = timezone.get_current_timezone()
     timezone_aware_date = current_timezone.localize(datetime_object)
 
-    new_aware_scheduled_meeting = ScheduledDate(date=timezone_aware_date, name=company_name)
-    new_aware_scheduled_meeting.save()
+    new_scheduled_meeting = ScheduledDate(date=timezone_aware_date, name=post_request["company_name"])
+    new_scheduled_meeting.save()
 
     return redirect(
         'schedule_success',
-        day=day,
-        month=month,
-        year=year,
-        hours=hours,
-        minutes=minutes,
-        name=company_name
+        post_request['day'],
+        post_request['month'],
+        post_request['year'],
+        post_request['hours'],
+        post_request['minutes'],
+        post_request['company_name'],
     )
 
 
@@ -117,13 +163,14 @@ def api_error(request, error_code):
     return render(request, 'api/schedule_error.html', {'error_code': error_code})
 
 
-def api_success(request, day, month, year, hours, minutes, name):
+def api_success(request, day, month, year, hours, minutes, company_name):
     return render(request, 'api/schedule_success.html', {
         'day': day,
         'month': month,
         'year': year,
         'hours': hours,
         'minutes': minutes,
-        'name': name,
+        'company_name': company_name
     })
+
 
