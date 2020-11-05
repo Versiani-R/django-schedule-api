8ff386ff22d0a3b5a76144cd6212fd0bb208e4ed
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2020-11-05 17:51:06 -0300

    Added django rest as an installed app in the settings.py in the root directory

--- src/api/views.py
+++ src/api/views.py
@@ -13,10 +13,10 @@ from .utils.validations import was_meeting_scheduled_to_a_saturday_or_sunday, \
     is_datetime_already_on_the_database
 
 from .authentication import generate_hash, generate_token
-from .models import ScheduledDate
+from .models import ScheduledDate, User
 from .threading import reset_api_calls_after_15_minutes
 
-from .spreadsheet import *
+# from .spreadsheet import *
 
 
 def index(request):
