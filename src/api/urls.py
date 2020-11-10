bcb83661f19847fbd9e6d9dc576010e837e612fd
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2020-11-10 09:11:00 -0300

    Added a new documentation file ( swagger ui ), go to swagger-ui/ to see it

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
@@ -18,5 +19,10 @@ urlpatterns = [
     path('docs/', get_schema_view(
         title='Schedule Api',
         description='A simple-core, well documented api, for schedules.'
-    ), name='openapi-schema')
+    ), name='openapi-schema'),
+
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='api/swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 ]
